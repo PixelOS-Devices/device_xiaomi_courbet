0b363eff0cf09b9df65c3ef860bca09c77d78a48
AdarshGrewal <adarshgrewal@gmail.com>
2022-01-25 14:03:13 -0500

    courbet: Include ANXCamera

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# ANXCamera
+$(call inherit-product, vendor/ANXCamera/anx-vendor.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
