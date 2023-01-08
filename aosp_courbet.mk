38e25a43371e3fcfc50beb2ec83e63f12582c731
basamaryan <basam.aryan@gmail.com>
2023-01-08 08:07:15 +0000

    Revert "courbet: Inherit correct makefile from our vendor"
    This reverts commit 95933e9d8fb100b8a4a9779194b7bc3c84b3d0ef.

--- aosp_courbet.mk
+++ aosp_courbet.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_NAME := aosp_courbet
