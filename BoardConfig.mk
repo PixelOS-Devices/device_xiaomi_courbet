8f725178914fb5e726d9b39c3b06b032d229e5fe
basamaryan <basam.aryan@gmail.com>
2023-07-16 20:16:30 +0530

    courbet: Switch to VantomKernel
    Change-Id: Ide81c968ad0ce29099ff63ff2d71fc6012386f0f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/courbet.config
+TARGET_KERNEL_CONFIG := courbet_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
