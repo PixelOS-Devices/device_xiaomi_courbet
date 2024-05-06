ec3a04c6b4658e9bf3b6987000e4ea2363c31cc8
Abdalrahman Eissa <abdalrahmaneissa@gmail.com>
2024-05-07 02:02:34 +0300

    Edit Device Fingerprint

--- init/init_courbet.cpp
+++ init/init_courbet.cpp
@@ -1,30 +1,7 @@
 /*
-   Copyright (C) 2020 The LineageOS Project.
 
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   Copyright (C) 2024 The LineageOS Project.
+   
  */
 
 #include <vector>
@@ -86,14 +63,14 @@ void vendor_load_properties() {
     std::string mod_device;
 
     if (region == "GLOBAL") {
-        model = "M2101K9AG";
+        model = "Xiaomi 11 Lite";
         device =  "courbet";
-        fingerprint = "Xiaomi/courbet/courbet:13/TKQ1.221013.002/V14.0.3.0.TKQMIXM:user/release-keys";
-        description = "courbet-user 13 TKQ1.221013.002 V14.0.3.0.TKQMIXM release-keys";
+        fingerprint = "Xiaomi/courbet/courbet:13/TKQ1.221013.002/V14.0.4.0.TKQMIXM:user/release-keys";
+        description = "courbet-user 13 TKQ1.221013.002 V14.0.4.0.TKQMIXM release-keys";
         mod_device = "courbet_global";
         property_override("ro.boot.product.hardware.sku", "courbet");
     } else if (region == "INDIA") {
-        model = "M2101K9AI";
+        model = "Xiaomi 11 Lite";
         device =  "courbetin"; 
         fingerprint = "Xiaomi/courbetin/courbetin:13/TKQ1.221013.002/V14.0.2.0.TKQINXM:user/release-keys";
         description = "courbetin-user 13 TKQ1.221013.002 V14.0.2.0.TKQINXM release-keys";
